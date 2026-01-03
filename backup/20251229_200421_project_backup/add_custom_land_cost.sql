02b8cf8a0696f7d8d31307c9481774e0bfadc261
clp2001 <clp2001@debian>
2026-01-04 01:11:08 +0800

    清理：删除所有旧备份和测试文件

--- backup/20251229_200421_project_backup/add_custom_land_cost.sql
+++
@@ -1,5 +0,0 @@
--- 添加custom_land_cost字段到investment_estimates表
-ALTER TABLE investment_estimates 
-ADD COLUMN custom_land_cost DECIMAL(15,2) DEFAULT NULL 
-COMMENT '自定义土地费用（万元）' 
-AFTER custom_loan_amount;
